12550dee9ba701b5523d359c2894b27ea4bd4acf
Talita Lopes <talitalopes@gmail.com>
2013-11-30 19:26:57 -0200

    Melhorias de código

--- ReuseProcessMining/ReuseProcessMiner/src/br/ufrj/cos/prisma/miner/Extractor/ReuseActionsExtractor.java
+++ ReuseProcessMining/ReuseProcessMiner/src/br/ufrj/cos/prisma/miner/Extractor/ReuseActionsExtractor.java
@@ -59,30 +59,31 @@ public class ReuseActionsExtractor {
 			return;
 		}
 
+		mProcess = process;
+		processActivities = new HashMap<String, Activity>();
+		eventsOrder = new ArrayList<String>();
+		frameworkProject = getFrameworkProject(process.getName());
+
+		if (frameworkProject == null) {
+			Log.i(Constants.ERROR_FRAMEWORK_NOT_EXISTS);
+			return;
+		}
+
+		Log.i(String.format("%s: %s", Constants.FRAMEWORK_PROJECT_NAME_KEY,
+				frameworkProject.getElementName()));
+
+		ArrayList<ProcessInstance> applications;
 		try {
-			mProcess = process;
-			processActivities = new HashMap<String, Activity>();
-			eventsOrder = new ArrayList<String>();
-			frameworkProject = getFrameworkProject(process.getName());
-
-			if (frameworkProject == null) {
-				Log.i(Constants.ERROR_FRAMEWORK_NOT_EXISTS);
-				return;
-			}
-
-			Log.i(String.format("%s: %s", Constants.FRAMEWORK_PROJECT_NAME_KEY,
-					frameworkProject.getElementName()));
-
 			// Add process instances to the process
-			ArrayList<ProcessInstance> applications = getFrameworkApplicationsCommits();
+			applications = getFrameworkApplicationsCommits();
 			process.getInstances().addAll(applications);
-
-		} catch (Exception e) {
+		} catch (CoreException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static ArrayList<ProcessInstance> getFrameworkApplicationsCommits() {
+	private static ArrayList<ProcessInstance> getFrameworkApplicationsCommits()
+			throws CoreException {
 		HashMap<String, ProcessInstance> applicationsMap = new HashMap<String, ProcessInstance>();
 		ArrayList<ProcessInstance> applications = new ArrayList<ProcessInstance>();
 
@@ -92,87 +93,100 @@ public class ReuseActionsExtractor {
 
 		// Get all projects in the workspace
 		IProject[] projects = root.getProjects();
-		try {
-			// Loop over all projects
-			for (IProject project : projects) {
 
-				project.open(null);
-				if (!project.isNatureEnabled(Constants.JAVA_NATURE)
-						|| (project.getName().equals(frameworkProject
-								.getElementName())))
-					continue;
-
-				IJavaProject javaProject = JavaCore.create(project);
-
-				if (javaProject.getElementName().split("\\.").length > 0) {
-					String[] keys = javaProject.getElementName().split("\\.");
-					String name = "";
-					for (int i = 0; i < keys.length - 2; i++) {
-						name = name + "." + keys[i];
-					}
-					String applicationName = name.replaceFirst("\\.", "");
-
-					SimpleDateFormat formatter = new SimpleDateFormat(
-							"yyyy-MM-dd");
-					Date commitDate = null;
-					try {
-						commitDate = formatter.parse(keys[keys.length - 2]);
-					} catch (ParseException e) {
-						commitDate = Calendar.getInstance().getTime();
-					}
-
-					Commit c = MinerFactory.eINSTANCE.createCommit();
-					c.eSet(MinerPackage.eINSTANCE.getCommit_Id(),
-							keys[keys.length - 1]);
-					c.eSet(MinerPackage.eINSTANCE.getCommit_Date(), commitDate);
-
-					// Add events already mined in ther right positions
-					for (String eventName : eventsOrder) {
-						Activity activity = processActivities.get(eventName);
-
-						if (activity == null) {
-							System.out.println("No activity for: " + eventName);
-							continue;
-						}
-
-						Event e = MinerFactory.eINSTANCE.createEvent();
-						e.setActivity(activity);
-						e.setDate(c.getDate());
-						e.setLifecycleStatus("COMPLETE");
-						c.getEvents().add(e);
-						System.out.println("pos: " + activity.getName());
-					}
-
-					ProcessInstance application = null;
-					if (applicationsMap.containsKey(applicationName)) {
-						application = applicationsMap.get(applicationName);
-					} else {
-						application = MinerFactory.eINSTANCE
-								.createProcessInstance();
-						application.eSet(
-								MinerPackage.eINSTANCE.getProcess_Name(),
-								applicationName);
-					}
-
-					application.getCommits().add(c);
-					applicationsMap.put(applicationName, application);
-
-					// Explore the files in this project
-					for (IPackageFragment mPackage : javaProject
-							.getPackageFragments()) {
-						explorePackage(mPackage, c);
-					}
-				}
+		// Loop over all projects
+		for (IProject project : projects) {
+			project.open(null);
+			if (!project.isNatureEnabled(Constants.JAVA_NATURE)
+					|| (project.getName().equals(frameworkProject
+							.getElementName()))) {
+				continue;
 			}
 
-			for (String name : applicationsMap.keySet()) {
-				applications.add(applicationsMap.get(name));
+			IJavaProject javaProject = JavaCore.create(project);
+			if (javaProject.getElementName().split("\\.").length == 0) {
+				continue;
 			}
-		} catch (CoreException e) {
-			e.printStackTrace();
+
+			String[] keys = javaProject.getElementName().split("\\.");
+			String name = "";
+			for (int i = 0; i < keys.length - 2; i++) {
+				name = name + "." + keys[i];
+			}
+			String applicationName = name.replaceFirst("\\.", "");
+
+			ProcessInstance application = null;
+			if (applicationsMap.containsKey(applicationName)) {
+				application = applicationsMap.get(applicationName);
+			} else {
+				application = MinerFactory.eINSTANCE.createProcessInstance();
+				application.eSet(MinerPackage.eINSTANCE.getProcess_Name(),
+						applicationName);
+			}
+			
+			Commit c = createCommit(keys);
+			application.getCommits().add(c);
+			applicationsMap.put(applicationName, application);
+
+			// Explore the files in this project
+			for (IPackageFragment mPackage : javaProject.getPackageFragments()) {
+				explorePackage(mPackage, c);
+			}
+		}
+
+		for (String name : applicationsMap.keySet()) {
+			applications.add(applicationsMap.get(name));
 		}
 
 		return applications;
+	}
+
+	private static Commit createCommit(String[] keys) {
+		Commit c = MinerFactory.eINSTANCE.createCommit();
+		c.eSet(MinerPackage.eINSTANCE.getCommit_Id(), keys[keys.length - 1]);
+		c.eSet(MinerPackage.eINSTANCE.getCommit_Date(),
+				getCommitDate(keys[keys.length - 2]));
+		addExistingEventsToCommit(c);
+
+		return c;
+	}
+
+	private static Date getCommitDate(String date) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		Date commitDate = null;
+		try {
+			commitDate = formatter.parse(date);
+		} catch (ParseException e) {
+			commitDate = Calendar.getInstance().getTime();
+		}
+		return commitDate;
+	}
+
+	private static void addExistingEventsToCommit(Commit c) {
+		for (String eventName : eventsOrder) {
+			String[] eventNameParts = eventName.split("\\+");
+			String eventKey = "";
+			if (eventNameParts.length == 2) {
+				eventKey = eventNameParts[0];
+			} else if (eventNameParts.length == 3) {
+				eventKey = String.format("%s+%s", eventNameParts[0], eventNameParts[2]);
+			}
+
+			Activity activity = processActivities.get(eventKey);
+			if (activity == null) {
+				System.out.println("No activity: " + eventName + ": " + eventKey);
+				continue;
+			}
+
+			Event e = MinerFactory.eINSTANCE.createEvent();
+			e.setActivity(activity);
+			e.setDate(c.getDate());
+			e.setLifecycleStatus("COMPLETE");
+			c.getEvents().add(e);
+			
+			System.out.println("Adding: " + eventName);
+		}
+		System.out.println("\n");
 	}
 
 	private static IJavaProject getFrameworkProject(String name) {
@@ -224,64 +238,44 @@ public class ReuseActionsExtractor {
 
 		for (ICompilationUnit unit : units) {
 			for (IType type : unit.getAllTypes()) {
-				if (type.isClass())
+				if (type.isClass()) {
 					extractClassAndMethods(type, c);
-				else if (type.isInterface())
+				} else if (type.isInterface()) {
 					continue; // TODO: get interfaces
+				}
 			}
 		}
 	}
 
-	private static void extractClassAndMethods(IType type, Commit c) {
-		try {
-			// Check is superclass belongs to framework.
-			IType superClassFW = isFrameworkClass(frameworkProject, type);
-			if (superClassFW == null) {
-				return;
-			}
-
-			Activity classExtensionActivity = getClassActivity(superClassFW,
-					type);
-
-			if (!eventsOrder.contains(classExtensionActivity.getName())) {
-				Event classExtensionEvent = MinerFactory.eINSTANCE
-						.createEvent();
-				classExtensionEvent.setActivity(classExtensionActivity);
-				classExtensionEvent.setDate(c.getDate());
-				classExtensionEvent.setLifecycleStatus("COMPLETE");
-
-				c.getEvents().add(classExtensionEvent);
-				eventsOrder.add(classExtensionActivity.getName());
-			}
-
-			IMethod[] methods = type.getMethods();
-
-			for (IMethod method : methods) {
-				boolean isExtension = superClassFW.findMethods(method) != null;
-				if (isExtension) {
-					Activity methodExtensionActivity = getMethodActivity(
-							superClassFW, type, method);
-					String methodKey = String.format("%s+%s",
-							superClassFW.getElementName(),
-							method.getElementName());
-					
-					if (!eventsOrder.contains(methodKey)) {
-						Event methodExtensionEvent = MinerFactory.eINSTANCE
-								.createEvent();
-						methodExtensionEvent
-								.setActivity(methodExtensionActivity);
-						methodExtensionEvent.setDate(c.getDate());
-						methodExtensionEvent.setLifecycleStatus("COMPLETE");
-
-						c.getEvents().add(methodExtensionEvent);
-
-						eventsOrder.add(methodKey);
-					}
-				}
-			}
-		} catch (JavaModelException e) {
-			e.printStackTrace();
+	private static void extractClassAndMethods(IType type, Commit c)
+			throws JavaModelException {
+		// Check is superclass belongs to framework.
+		IType superClassFW = isFrameworkClass(frameworkProject, type);
+		if (superClassFW == null) {
+			return;
 		}
+
+		Activity classExtensionActivity = getClassActivity(superClassFW, type);
+		String activityNameKey = String.format("%s+%s",
+				superClassFW.getFullyQualifiedName(), type.getElementName());
+
+		if (eventsOrder.contains(activityNameKey)) {
+			System.out.println("contains: " + activityNameKey);
+			return;
+		}
+
+		System.out.println("activityNameKey: "
+				+ classExtensionActivity.getName());
+		Event classExtensionEvent = MinerFactory.eINSTANCE.createEvent();
+		classExtensionEvent.setActivity(classExtensionActivity);
+		classExtensionEvent.setDate(c.getDate());
+		classExtensionEvent.setLifecycleStatus("COMPLETE");
+
+		c.getEvents().add(classExtensionEvent);
+		eventsOrder.add(activityNameKey);
+		System.out.println("Adding: " + activityNameKey);
+
+		getClassMethods(superClassFW, type, c);
 
 		/*
 		 * try { log(String.format("Type: %s", type.getElementName()));
@@ -302,95 +296,112 @@ public class ReuseActionsExtractor {
 		 */
 	}
 
+	private static void getClassMethods(IType superClassFW, IType type, Commit c)
+			throws JavaModelException {
+		IMethod[] methods = type.getMethods();
+
+		for (IMethod method : methods) {
+			boolean isExtension = superClassFW.findMethods(method) != null;
+			if (!isExtension) {
+				continue;
+			}
+			Activity methodExtensionActivity = getMethodActivity(superClassFW,
+					type, method);
+
+			// Super class + method + class name
+			String methodKey = String.format("%s+%s+%s",
+					superClassFW.getElementName(), type.getElementName(),
+					method.getElementName());
+			if (eventsOrder.contains(methodKey)) {
+				System.out.println("contains: " + methodKey);
+				continue;
+			}
+
+			Event methodExtensionEvent = MinerFactory.eINSTANCE.createEvent();
+			methodExtensionEvent.setActivity(methodExtensionActivity);
+			methodExtensionEvent.setDate(c.getDate());
+			methodExtensionEvent.setLifecycleStatus("COMPLETE");
+			c.getEvents().add(methodExtensionEvent);
+
+			eventsOrder.add(methodKey);
+			System.out.println("Adding: " + methodKey);
+		}
+	}
+
 	private static Activity getClassActivity(IType superClassFW, IType type) {
 		Activity classExtensionActivity = null;
 		String activityKey = superClassFW.getFullyQualifiedName();
 
-		if (!processActivities.containsKey(activityKey)) {
-			classExtensionActivity = MinerFactory.eINSTANCE.createActivity();
-			classExtensionActivity.setId(activityKey);
-			classExtensionActivity.setType(ActivityType.CLASS_EXTENSION);
-			classExtensionActivity
-					.setName(superClassFW.getFullyQualifiedName());
-			classExtensionActivity.setAppClass(type.getElementName());
-			// classRA.setPosition(nextPosition);
-
-			mProcess.getActivities().add(classExtensionActivity);
-			processActivities.put(activityKey, classExtensionActivity);
-
-		} else {
-			classExtensionActivity = processActivities.get(activityKey);
+		if (processActivities.containsKey(activityKey)) {
+			return processActivities.get(activityKey);
 		}
+
+		classExtensionActivity = MinerFactory.eINSTANCE.createActivity();
+		classExtensionActivity.setId(activityKey);
+		classExtensionActivity.setType(ActivityType.CLASS_EXTENSION);
+		classExtensionActivity.setName(superClassFW.getFullyQualifiedName());
+		classExtensionActivity.setAppClass(type.getElementName());
+		mProcess.getActivities().add(classExtensionActivity);
+		processActivities.put(activityKey, classExtensionActivity);
 
 		return classExtensionActivity;
 	}
 
 	private static Activity getMethodActivity(IType superClassFW, IType type,
-			IMethod method) {
+			IMethod method) throws JavaModelException {
 		Activity methodExtensionActivity = null;
+		String methodKey = String.format("%s+%s",
+				superClassFW.getElementName(), method.getElementName());
 
-		try {
-			String methodKey = String.format("%s+%s",
-					superClassFW.getElementName(), method.getElementName());
-			if (!processActivities.containsKey(methodKey)) {
-				methodExtensionActivity = MinerFactory.eINSTANCE
-						.createActivity();
-				methodExtensionActivity.setId(methodKey);
-
-				if (method.getSource().contains(
-						"super." + method.getElementName() + "()")) {
-					methodExtensionActivity
-							.setType(ActivityType.METHOD_EXTENSION);
-				} else {
-					methodExtensionActivity
-							.setType(ActivityType.OVERRIDES_METHOD);
-				}
-
-				methodExtensionActivity.setName(method.getElementName());
-				methodExtensionActivity.setAppClass(type.getElementName());
-
-				mProcess.getActivities().add(methodExtensionActivity);
-				processActivities.put(methodKey, methodExtensionActivity);
-
-			} else {
-				methodExtensionActivity = processActivities.get(methodKey);
-			}
-		} catch (JavaModelException e) {
-			e.printStackTrace();
+		if (processActivities.containsKey(methodKey)) {
+			return processActivities.get(methodKey);
 		}
+
+		methodExtensionActivity = MinerFactory.eINSTANCE.createActivity();
+		methodExtensionActivity.setId(methodKey);
+
+		if (method.getSource().contains(
+				"super." + method.getElementName() + "()")) {
+			methodExtensionActivity.setType(ActivityType.METHOD_EXTENSION);
+		} else {
+			methodExtensionActivity.setType(ActivityType.OVERRIDES_METHOD);
+		}
+
+		methodExtensionActivity.setName(method.getElementName());
+		methodExtensionActivity.setAppClass(type.getElementName());
+		mProcess.getActivities().add(methodExtensionActivity);
+		processActivities.put(methodKey, methodExtensionActivity);
 
 		return methodExtensionActivity;
 	}
 
-	public static IType isFrameworkClass(IJavaProject framework, IType type) {
-		try {
-			if (type == null || type.getSuperclassName() == null)
-				return null;
+	public static IType isFrameworkClass(IJavaProject framework, IType type)
+			throws JavaModelException {
+		if (framework == null) {
+			return null;
+		}
 
-			if (framework == null)
-				return null;
+		if (type == null || type.getSuperclassName() == null) {
+			return null;
+		}
 
-			IType superClass = null;
-			if (type.getCompilationUnit() != null) {
-				IImportDeclaration[] imports = type.getCompilationUnit()
-						.getImports();
-				for (int i = 0; i < imports.length; i++) {
-					if (imports[i].getElementName() == null)
-						continue;
+		IType superClass = null;
+		if (type.getCompilationUnit() == null) {
+			return null;
+		}
 
-					// TODO: generalize this code!!!
-					if (imports[i].getElementName().contains(
-							type.getSuperclassName())
-							&& imports[i].getElementName().contains("gef")) {
-						superClass = framework.findType(imports[i]
-								.getElementName());
-						return superClass;
-					}
-				}
+		IImportDeclaration[] imports = type.getCompilationUnit().getImports();
+		for (int i = 0; i < imports.length; i++) {
+			if (imports[i].getElementName() == null) {
+				continue;
 			}
 
-		} catch (JavaModelException e) {
-			e.printStackTrace();
+			// TODO: generalize this code!!!
+			if (imports[i].getElementName().contains(type.getSuperclassName())
+					&& imports[i].getElementName().contains("gef")) {
+				superClass = framework.findType(imports[i].getElementName());
+				return superClass;
+			}
 		}
 
 		return null;
