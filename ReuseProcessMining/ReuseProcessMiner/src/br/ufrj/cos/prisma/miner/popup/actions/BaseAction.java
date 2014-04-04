9d9e2bf8deb23a8a1c9c7cf63353d48b6e3ceaab
Talita Lopes <talitalopes@gmail.com>
2014-04-03 23:26:17 -0300

    Adicionada BaseAction que é classe base para as ações do plugin

--- ReuseProcessMining/ReuseProcessMiner/src/br/ufrj/cos/prisma/miner/popup/actions/BaseAction.java
+++ ReuseProcessMining/ReuseProcessMiner/src/br/ufrj/cos/prisma/miner/popup/actions/BaseAction.java
@@ -1,52 +1,54 @@
 package br.ufrj.cos.prisma.miner.popup.actions;
 
 import java.io.IOException;
-import java.util.List;
 
-import minerv1.FrameworkApplication;
 import minerv1.FrameworkProcess;
-import minerv1.Minerv1Factory;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.command.BasicCommandStack;
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
 
-import br.ufrj.cos.prisma.helpers.RepositoriesHelper;
-import br.ufrj.cos.prisma.model.GithubRepository;
+public class BaseAction implements IObjectActionDelegate {
+	protected Shell shell;
+	protected IWorkbenchPart part;
+	protected FrameworkProcess process;
+	protected Resource resource;
+	protected ComposedAdapterFactory composedAdapterFactory;
 
-public class ListRepositoriesAction extends BaseAction {
-	public ListRepositoriesAction() {
+	public BaseAction() {
 		super();
 	}
 
 	@Override
-	public void run(IAction action) {
-		super.run(action);		
-		listRepositories();
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+		part = targetPart;
 	}
 
-	private List<GithubRepository> listRepositories() {
-		List<GithubRepository> repositories = RepositoriesHelper
-				.listRepositories("graphiti"); // "JJTV5_gef"
-		// create applications for repositories
-		for (GithubRepository repo : repositories) {
-			FrameworkApplication app = Minerv1Factory.eINSTANCE
-					.createFrameworkApplication();
-			app.setName(repo.getName());
-			app.setRepositoryUrl(repo.getCloneUrl());
-			System.out.println("Add application " + app.getName()
-					+ " to process " + process.getName());
-			this.process.getApplications().add(app);
-		}
-		
-		save();
+	@Override
+	public void run(IAction arg0) {
+		ISelection sel = part.getSite().getSelectionProvider().getSelection();
 
-		return repositories;
+		if (sel instanceof TreeSelection) {
+			TreeSelection treeSelection = (TreeSelection) sel;
+			Object firstElement = treeSelection.getFirstElement();
+
+			if (firstElement instanceof IFile) {
+				System.out.println("Loading process");
+				loadModel((IFile) firstElement);
+			}
+		}
+
 	}
 
 	public void loadModel(IFile model) {
